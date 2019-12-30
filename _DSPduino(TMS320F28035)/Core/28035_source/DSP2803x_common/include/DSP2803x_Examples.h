56df310f9b06fdb8898ba6732345b850eb328215
_VIFEXTech <1290176185@qq.com>
2019-12-30 12:52:42 +0800

    主频已正常

--- _DSPduino(TMS320F28035)/Core/28035_source/DSP2803x_common/include/DSP2803x_Examples.h
+++ _DSPduino(TMS320F28035)/Core/28035_source/DSP2803x_common/include/DSP2803x_Examples.h
@@ -28,15 +28,15 @@ extern "C" {
 //#define DSP28_DIVSEL   3 // Enable /1 for SYSCLKOUT
 
 
-//#define DSP28_PLLCR   12    // Uncomment for 60 MHz devices [60 MHz = (10MHz * 12)/2]
+#define DSP28_PLLCR   12    // Uncomment for 60 MHz devices [60 MHz = (10MHz * 12)/2]
 //#define DSP28_PLLCR   11
 //#define DSP28_PLLCR   10
 //#define DSP28_PLLCR    9
-//#define DSP28_PLLCR    8      // Uncomment for 40 MHz devices [40 MHz = (10MHz * 8)/2]
+//#define DSP28_PLLCR    8  // Uncomment for 40 MHz devices [40 MHz = (10MHz * 8)/2]
 //#define DSP28_PLLCR    7
 //#define DSP28_PLLCR    6
 //#define DSP28_PLLCR    5
-#define DSP28_PLLCR    4
+//#define DSP28_PLLCR    4
 //#define DSP28_PLLCR    3
 //#define DSP28_PLLCR    2
 //#define DSP28_PLLCR    1
